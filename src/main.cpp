891f2646de67ca8c4d31ae29bd1c2f66be2b72cb
Bean_siuuu <yojohn756@gmail.com>
2026-07-13 06:49:44 +0800

    Prototype using esp32-s3-wroom cam, only basic functions, not verified

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@
 
 //-------------------------------------
 // LED strip settings
-#define LED_PIN 15 // no #CS(GPIO15) required
+#define LED_PIN 14 // no #CS(GPIO14) required
 #define NUM_LEDS 258
 #define STRIP_WIDTH 84 // number of LEDS
 #define STRIP_HEIGHT 45 // number of LEDS
@@ -51,23 +51,23 @@ SemaphoreHandle_t frameMutex = nullptr;
 // FreeRTOS task handles
 TaskHandle_t cameraTaskHandle = nullptr;
 
-// ESP32Cam (AiThinker) PIN Map
-#define CAM_PIN_PWDN 32
-#define CAM_PIN_RESET -1 //software reset will be performed
-#define CAM_PIN_XCLK 0
-#define CAM_PIN_SIOD 26
-#define CAM_PIN_SIOC 27
-#define CAM_PIN_D7 35
-#define CAM_PIN_D6 34
-#define CAM_PIN_D5 39
-#define CAM_PIN_D4 36
-#define CAM_PIN_D3 21
-#define CAM_PIN_D2 19
-#define CAM_PIN_D1 18
-#define CAM_PIN_D0 5
-#define CAM_PIN_VSYNC 25
-#define CAM_PIN_HREF 23
-#define CAM_PIN_PCLK 22
+// Freenove ESP32-S3 WROOM CAM PIN Map
+#define CAM_PIN_PWDN  -1
+#define CAM_PIN_RESET -1
+#define CAM_PIN_XCLK  15
+#define CAM_PIN_SIOD  4
+#define CAM_PIN_SIOC  5
+#define CAM_PIN_D7    16  // CSI_Y9
+#define CAM_PIN_D6    17  // CSI_Y8
+#define CAM_PIN_D5    18  // CSI_Y7
+#define CAM_PIN_D4    12  // CSI_Y6
+#define CAM_PIN_D3    10  // CSI_Y5
+#define CAM_PIN_D2    8   // CSI_Y4
+#define CAM_PIN_D1    9   // CSI_Y3
+#define CAM_PIN_D0    11  // CSI_Y2
+#define CAM_PIN_VSYNC 6
+#define CAM_PIN_HREF  7
+#define CAM_PIN_PCLK  13
 
 // Log module tag
 static const char *TAG = "ambilight";
@@ -78,8 +78,6 @@ static camera_config_t camera_config = {
     .pin_xclk = CAM_PIN_XCLK,
     .pin_sccb_sda = CAM_PIN_SIOD,
     .pin_sccb_scl = CAM_PIN_SIOC,
-    
-
     .pin_d7 = CAM_PIN_D7,
     .pin_d6 = CAM_PIN_D6,
     .pin_d5 = CAM_PIN_D5,
@@ -96,16 +94,14 @@ static camera_config_t camera_config = {
     .xclk_freq_hz = 10000000,
     .ledc_timer = LEDC_TIMER_0,
     .ledc_channel = LEDC_CHANNEL_0,
-    
-    //.ledc_timer = LEDC_TIMER_1,
-    //.ledc_channel = LEDC_CHANNEL_1,
-
+ 
     .pixel_format = PIXFORMAT_RGB565, //YUV422,GRAYSCALE,RGB565,JPEG
     
     .frame_size = FRAMESIZE_QQVGA,    //QQVGA-UXGA, For ESP32, do not use sizes above QVGA when not JPEG. The performance of the ESP32-S series has improved a lot, but JPEG mode always gives better frame rates.
 
     .fb_count = 1,       //When jpeg mode is used, if fb_count more than one, the driver will work in continuous mode.
-    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+    .fb_location = CAMERA_FB_IN_PSRAM,
+	.grab_mode = CAMERA_GRAB_LATEST, //For this project skipping some frames is acceptable, but getting the latest frame is important.
     
 };
 
